bf16f9dd73b4f2198f4a94b026d97b938003e545
Anibire Azeez <ademolaa202@gmail.com>
2022-07-03 14:57:20 -0700

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /
 * main - Entry point
-* return:Always 0 (sucess)
+* Return: Always 0 (sucess)
 */
 int main(void)
 {
-	puts("/"Programming is lke building a multilingual puzzle");
-	return(0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
