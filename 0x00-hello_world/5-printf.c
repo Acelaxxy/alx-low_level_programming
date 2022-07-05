debafab02295e9784288cf49949aa288928948bf
Anibire Azeez <ademolaa202@gmail.com>
2022-07-04 22:08:37 -0700

    5. Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
-/*
- * main - Entry point
+/**
+ * main - with proper grammar, but the outcome is a piece of art
  *
- * Return: Always 0
+ * Return: Always 0 (success)
  */
+
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
+	printf("with proper grammar, but the outcome is a piece of art \n");
 	return (0);
 }
