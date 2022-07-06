8e7c74a5ae2c149a5d5e65e2a380ec31f451a680
Anibire Azeez <ademolaa202@gmail.com>
2022-07-05 17:18:06 -0700

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints alphabets times 10
+ *
+ * Return: Always 0
+ */
+
+void print_alphabet_x10(void)
+{
+	int i = 0;
+	char c;
+
+	while (i++; i <= 9)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+	}
+}
