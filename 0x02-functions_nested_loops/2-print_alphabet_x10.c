74bb97204b0a5d5ab73f045a43f0314959ab8a60
Anibire Azeez <ademolaa202@gmail.com>
2022-07-06 00:26:59 -0700

    10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,12 +10,13 @@ void print_alphabet_x10(void)
 	int i = 0;
 	char c;
 
-	while (i <= 9; i++)
+	while (i <= 9)
 	{
 		for (c = 'a'; c <= 'z'; c++)
 		{
 			_putchar(c);
 		}
 		_putchar('\n');
+		i++;
 	}
 }
