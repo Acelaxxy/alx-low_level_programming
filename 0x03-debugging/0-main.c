399bec8327210a4d628ef20148d64caf9f9cf095
Anibire Azeez <ademolaa202@gmail.com>
2022-07-07 11:31:22 -0700

    Multiple mains

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - calls other functions
+ *
+ * return: 0
+ */
+
+void positive_or_negative(void);
+
+int main(void)
+{
+	positive_or_negative();
+
+	return (0);
+}
