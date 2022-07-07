747e94cb5026c0b5426e73ef974ea01bcaed0cf7
Anibire Azeez <ademolaa202@gmail.com>
2022-07-07 10:35:34 -0700

    Collaboration is multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - Checks if an alphabet is upper or not
+ * @a: The integer that multiply b
+ * @b: The integer that multiply a
+ *
+ * Return: Product of a and b
+ */
+
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+
+	return (c);
+}
