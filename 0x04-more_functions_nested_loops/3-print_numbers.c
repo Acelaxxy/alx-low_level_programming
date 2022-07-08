65c4d99b57b2906ed55c3d857d5007ce67b5fa6c
Anibire Azeez <ademolaa202@gmail.com>
2022-07-08 11:39:21 -0700

    The numbers speaks for themselves

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -11,9 +11,9 @@ void print_numbers(void)
 {
 	char c;
 
-	for (c = 0; c < 10; c++)
+	for (c = 0; c <= 9; c++)
 	{
-		_putchar((c % 10) + '0');
-		_putchar('\n');
+		_putchar(c);
 	}
+		_putchar('\n');
 }
