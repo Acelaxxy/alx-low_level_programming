b5be2129b1a5277f761d0bcf7c9e0635f703c234
Anibire Azeez <ademolaa202@gmail.com>
2022-07-10 19:31:58 -0700

    Patience, persistence and perspiration make an unbeatable combination for success

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Prints the all combinations of number followed by a new line
+ *
+ * Return: Always 0 (if successful) and 1 (otherwise)
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num <= 9; num++)
+	{
+		putchar((num % 10) + '0');
+	if (num == 9)
+		continue;
+	putchar(',');
+	putchar(' ');
+	}
+	putchar('\n');
+
+	return (0);
+}
+
