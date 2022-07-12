bbdefcb53446c67cc5f81f375a813b2e64e46d31
Anibire Azeez <ademolaa202@gmail.com>
2022-07-12 10:19:24 -0700

    98, Battery street

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Takes a pointer to an int
+ * @n: The integer we want to point to 98
+ *
+ * Return: Always 0.
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+
+}
