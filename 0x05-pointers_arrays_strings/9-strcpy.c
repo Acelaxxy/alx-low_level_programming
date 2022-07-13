ea7ba90511f4a63cfb0d80e13ee7778ca5a6980e
Anibire Azeez <ademolaa202@gmail.com>
2022-07-13 09:38:39 -0700

    strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * *_strcpy - Copies the string of a pointed src
+ * @dest: The integer to be printed
+ * @src: The number of array elements
+ * Description: This will copy the pointed src of a string
+ * Return: Always 0.
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i = -1;
+
+	do {
+		dest[i] = src[i];
+	} while (src[i] != '\0');
+
+	return (dest);
+}
